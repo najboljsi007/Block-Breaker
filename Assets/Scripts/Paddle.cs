5f3a96a3424113b09e99b4b8fc8ad806576ee433
Martin Knavs <martin.knavs@outfit7.com>
2018-10-05 20:47:32 +0200

    Udemy 5/53
    Clamp paddle to the left and right screen border

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
     // screenWidth is defined by camera size in world units
     [SerializeField] float screenWidthInUnits = 16f;
+    // configuration parameters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
 
     // Use this for initialization
     void Start () {
@@ -14,7 +17,8 @@ public class Paddle : MonoBehaviour {
 	void Update () {
         float mouseXPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
 
-        Vector2 paddlePosition = new Vector2(mouseXPos, transform.position.y);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mouseXPos, minX, maxX);
         transform.position = paddlePosition;
 	}
 }
