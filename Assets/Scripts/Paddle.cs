c74955135e01be2305f19ad75ca89f87cdbec9a7
Martin Knavs <martin.knavs@outfit7.com>
2018-10-05 18:39:23 +0200

    Udemy: 5/52
    Move paddle with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    // screenWidth is defined by camera size in world units
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	void Update () {
+        float mouseXPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        Vector2 paddlePosition = new Vector2(mouseXPos, transform.position.y);
+        transform.position = paddlePosition;
+	}
+}
