dc183c74726c4ecd4ea7ca0157325e298a63e0e6
Martin Knavs <martin.knavs@outfit7.com>
2018-10-05 21:11:00 +0200

    Udemy: 5/54
    Stick ball to paddle, get readt for the first launch

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    [SerializeField] Paddle paddle;
+    Vector2 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
