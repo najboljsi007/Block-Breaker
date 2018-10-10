785335e68ec09d341bc0566c2fc955fd6ffcdb0d
Martin Knavs <martin.knavs@outfit7.com>
2018-10-10 18:10:03 +0200

    Udemy: 5/65
    Load new level when all block destroyed in scene

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Level : MonoBehaviour {
 
+    // parameters
     [SerializeField] int breakableBlocks; // Serialized for debugging purposes
+
+    // cached scene
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
 
     public void CountBreakableBlocks() {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed(){
+        breakableBlocks--;
+        if (breakableBlocks <= 0){
+            sceneLoader.LoadNextScene();
+        }
     }
 
 }
